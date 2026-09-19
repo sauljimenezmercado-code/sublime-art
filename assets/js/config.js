04fdcfcded3a8ae3e25a1bfa6ceb1f5d387473ac
sauljimenezmercado-code <sauljimenezmercado@gmail.com>
2026-09-19 16:52:31 -0600

    Publicar SUBLIME ART versión 5

---
+++ assets/js/config.js
@@ -0,0 +1,5 @@
+export const SITE_CONFIG = {
+  whatsappPhone: "", // PENDIENTE: agregar número real en formato internacional, sin + ni espacios
+  brandName: "SUBLIME ART",
+  defaultMessage: "Hola, me gustaría recibir información sobre sus productos personalizados."
+};
---
+++ assets/js/products-data.js
@@ -0,0 +1,58 @@
+export const CATEGORIES = [
+  { id: "playeras", name: "Playeras", number: "01", image: "assets/img/productos/playeras/playera-demo.jpg" },
+  { id: "tazas", name: "Tazas", number: "02", image: "assets/img/productos/tazas/taza-demo.jpg" },
+  { id: "termos", name: "Termos", number: "03", image: "assets/img/productos/termos/termo-demo.jpg" },
+  { id: "sombrillas", name: "Sombrillas", number: "04", image: "assets/img/productos/sombrillas/sombrilla-demo.jpg" }
+];
+
+export const PRODUCTS = [
+  {
+    id: "prod-0001",
+    name: "Playera personalizada",
+    category: "playeras",
+    description: "Una base para llevar tus ideas a donde quieras.",
+    image: "assets/img/productos/playeras/playera-demo.jpg",
+    available: true,
+    price: null,
+    whatsappNote: ""
+  },
+  {
+    id: "prod-0002",
+    name: "Taza personalizada",
+    category: "tazas",
+    description: "Un detalle cotidiano con tu propio estilo.",
+    image: "assets/img/productos/tazas/taza-demo.jpg",
+    available: true,
+    price: null,
+    whatsappNote: ""
+  },
+  {
+    id: "prod-0003",
+    name: "Termo personalizado",
+    category: "termos",
+    description: "Personalización para acompañar tu día.",
+    image: "assets/img/productos/termos/termo-demo.jpg",
+    available: true,
+    price: null,
+    whatsappNote: ""
+  },
+  {
+    id: "prod-0004",
+    name: "Sombrilla personalizada",
+    category: "sombrillas",
+    description: "Una opción funcional para expresar tu idea.",
+    image: "assets/img/productos/sombrillas/sombrilla-demo.jpg",
+    available: true,
+    price: null,
+    whatsappNote: ""
+  }
+];
+
+export const GALLERY_ITEMS = [
+  { title: "Detalle de personalización", image: "assets/img/galeria/galeria-01.jpg" },
+  { title: "Producto terminado", image: "assets/img/galeria/galeria-02.jpg" },
+  { title: "Composición de productos", image: "assets/img/galeria/galeria-03.jpg" },
+  { title: "Diseño aplicado", image: "assets/img/galeria/galeria-04.jpg" },
+  { title: "Presentación de pedido", image: "assets/img/galeria/galeria-05.jpg" },
+  { title: "Idea convertida en producto", image: "assets/img/galeria/galeria-06.jpg" }
+];
